c092eb8a467c3e61cffa383ab7e88e7ac627bc3e
glavender <glavender@GLAVENDER17-DE>
2017-05-15 11:20:11 +0200

    maj

---
+++ OptionWord/UCGénéral.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OptionWord
+{
+    /// <summary>
+    /// Interaction logic for UCGénéral.xaml
+    /// </summary>
+    public partial class UCGénéral : UserControl
+    {
+        public UCGénéral()
+        {
+            InitializeComponent();
+        }
+    }
+}
