e08fe3be205470b7bca071abf5b944eb5ffa30d1
glavender <glavender@GLAVENDER17-DE>
2017-05-15 17:09:44 +0200

    maj

---
+++ RelevésMétéo/LstNameToDetailsNameConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RelevésMétéo
+{
+    public class LstNameToDetailsNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter.ToString() == "DétailVignettes")
+                return value.ToString() == "ListMétéo" ? "Visible" : "Hidden";
+            else
+                return value.ToString() == "ListMétéo" ? "Hidden" : "Visible";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DecimalToColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double d = (double)value;
+            double seuil = (double)parameter;
+            Color c = (d <= seuil ? Colors.LightYellow : Colors.White);
+            return new SolidColorBrush(c);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ RelevésMétéo/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RelevésMétéo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DALMeteo Météo { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Météo = new DALMeteo();
+            BtnPath.Click += BtnPath_Click;
+            CBMétéo.SelectionChanged += LstBoxMétéo_SelectionChanged;
+            
+        }
+
+        private void LstBoxMétéo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+                LstBoxMétéo.ItemTemplate = (DataTemplate)Resources[CBMétéo.SelectedValue];
+        }
+
+        private void BtnPath_Click(object sender, RoutedEventArgs e)
+        {
+            var dial = new Microsoft.Win32.OpenFileDialog();
+            dial.ShowDialog();
+            try
+            {
+                Météo.ChargerDonnées(dial.FileName);
+                window.DataContext = Météo;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ce fichier n'est pas valide");                
+            }
+
+        }
+    }
+}
